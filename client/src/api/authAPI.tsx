25a09eb03556501ae85e0dcbc5e1f70135e13046
ellimckinley <190907228+ellimckinley@users.noreply.github.com>
2025-03-29 12:26:49 -0600

    completed post request in authAPI.tsx file

--- client/src/api/authAPI.tsx
+++ client/src/api/authAPI.tsx
@@ -1,9 +1,33 @@
 import { UserLogin } from "../interfaces/UserLogin";
 
 const login = async (userInfo: UserLogin) => {
-  // TODO: make a POST request to the login route
-}
+  // TODO: make a POST request to the login route 
+  try {
+    const response = await fetch(
+      '/api/users/', {
+        method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        body: JSON.stringify(userInfo)
+      }
 
+    )
+    const data = await response.json();
 
+    if(!response.ok) {
+      throw new Error(data.message || 'invalid API response, check network tab!');
+    }
+
+    return data;
+
+  } catch (err) {
+    console.log('Error from Work Creation: ', err);
+    if (err instanceof Error) {
+      return Promise.reject(err.message || 'Could not create new work');
+    }
+    return Promise.reject('Could not create new work');
+  }
+};
 
 export { login };
