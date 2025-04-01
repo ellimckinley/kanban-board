d07b1592595021fd9c49cf999786c334965d836d
ellimckinley <190907228+ellimckinley@users.noreply.github.com>
2025-03-31 22:25:11 -0600

    render connect to postgresql

--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -5,20 +5,37 @@ import { Sequelize } from "sequelize";
 import { UserFactory } from "./user.js";
 import { TicketFactory } from "./ticket.js";
 
-const sequelize = process.env.DB_URL
-  ? new Sequelize(process.env.DB_URL)
-  : new Sequelize(
-      process.env.DB_NAME || "",
-      process.env.DB_USER || "",
-      process.env.DB_PASSWORD,
-      {
-        host: "localhost",
-        dialect: "postgres",
-        dialectOptions: {
-          decimalNumbers: true,
-        },
-      },
-    );
+// const sequelize = process.env.DB_URL
+//   ? new Sequelize(process.env.DB_URL)
+//   : new Sequelize(
+//       process.env.DB_NAME || "",
+//       process.env.DB_USER || "",
+//       process.env.DB_PASSWORD,
+//       {
+//         host: "localhost",
+//         dialect: "postgres",
+//         dialectOptions: {
+//           decimalNumbers: true,
+//         },
+//       },
+//     );
+
+// setting db up for Render
+let sequelize;
+
+if (process.env.DB_URL) {
+  sequelize = new Sequelize(process.env.DB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME || "",
+    process.env.DB_USER || "",
+    process.env.DB_PW,
+    {
+      host: 'localhost',
+      dialect: 'postgres',
+    },
+  );
+}
 
 const User = UserFactory(sequelize);
 const Ticket = TicketFactory(sequelize);
